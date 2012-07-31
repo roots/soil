555920dacb77f0d43cf545993142bb8a7b66d244
Ben Word <ben@benword.com>
2012-07-30 19:10:37 -0500

    Initial commit

---
+++ mu-plugins/load.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Description: Options Framework, Stresslimit Custom Content & Taxonomies, Custom Metaboxes and Fields, CF Post Formats
+*/
+require WPMU_PLUGIN_DIR . '/options-framework-plugin/options-framework.php';
+require WPMU_PLUGIN_DIR . '/sld-custom-content-and-taxonomies/sld-custom-content-and-taxonomies.php';
+require WPMU_PLUGIN_DIR . '/cmb/init.php';
+require WPMU_PLUGIN_DIR . '/wp-post-formats/cf-post-formats.php';
