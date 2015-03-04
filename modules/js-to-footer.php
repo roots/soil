b88062783eca0a04609015f044d2d33deefc0dc3
QWp6t <hi@qwp6t.me>
2015-03-04 12:22:15 -0800

    Pass namespace to action hook in js-to-footer

--- modules/js-to-footer.php
+++ modules/js-to-footer.php
@@ -13,4 +13,4 @@ function js_to_footer() {
   remove_action('wp_head', 'wp_print_head_scripts', 9);
   remove_action('wp_head', 'wp_enqueue_scripts', 1);
 }
-add_action('wp_enqueue_scripts', 'js_to_footer');
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\js_to_footer');
