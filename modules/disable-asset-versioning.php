3fa9efb24b0401f5623d21fe04738b1d0fac8424
Kalen Johnson <info@kalenjohnson.com>
2015-04-21 12:11:55 -0700

    Escaping remove_query_arg, also fixing spacing

--- modules/disable-asset-versioning.php
+++ modules/disable-asset-versioning.php
@@ -8,8 +8,8 @@ namespace Roots\Soil\DisableAssetVersioning;
  * You can enable/disable this feature in functions.php (or lib/config.php if you're using Sage):
  * add_theme_support('soil-disable-asset-versioning');
  */
-function remove_script_version($src){
-  return remove_query_arg('ver', $src);
+function remove_script_version($src) {
+  return esc_url(remove_query_arg('ver', $src));
 }
 add_filter('script_loader_src', __NAMESPACE__ . '\\remove_script_version', 15, 1);
 add_filter('style_loader_src', __NAMESPACE__ . '\\remove_script_version', 15, 1);
