990453de008ed648431e3080a52dc157d163fc8f
QWp6t <hi@qwp6t.me>
2015-06-29 19:26:14 -0700

    Fix Disable Asset Ver when loaded asset is false
    Sometimes the `$src` passed to `remove_script_version($src)` is `false`. In these instances, it should return false instead of a URL.

--- modules/disable-asset-versioning.php
+++ modules/disable-asset-versioning.php
@@ -9,7 +9,7 @@ namespace Roots\Soil\DisableAssetVersioning;
  * add_theme_support('soil-disable-asset-versioning');
  */
 function remove_script_version($src) {
-  return esc_url(remove_query_arg('ver', $src));
+  return $src ? esc_url(remove_query_arg('ver', $src)) : false;
 }
 add_filter('script_loader_src', __NAMESPACE__ . '\\remove_script_version', 15, 1);
 add_filter('style_loader_src', __NAMESPACE__ . '\\remove_script_version', 15, 1);
