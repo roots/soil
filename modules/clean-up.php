24894e33227ac1ec26924c1fd573d1cfd06d0fe5
QWp6t <hi@qwp6t.me>
2015-12-09 13:10:44 -0800

    Remove `wp-embed.js` and `wp-json` from head, closes #129

--- modules/clean-up.php
+++ modules/clean-up.php
@@ -31,6 +31,8 @@ function head_cleanup() {
   remove_action('admin_print_scripts', 'print_emoji_detection_script');
   remove_action('wp_print_styles', 'print_emoji_styles');
   remove_action('admin_print_styles', 'print_emoji_styles');
+  remove_action('wp_head', 'wp_oembed_add_host_js');
+  remove_action('wp_head', 'rest_output_link_wp_head', 10, 0);
   remove_filter('the_content_feed', 'wp_staticize_emoji');
   remove_filter('comment_text_rss', 'wp_staticize_emoji');
   remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
