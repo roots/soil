10fa7fd03dd0dba25b30354fcf14f37da98ded26
Sam Edwards <sam@thenumber.ca>
2017-05-04 12:32:40 -0700

    Do not proceed with custom post type `active` url check if the CPT does not have an archive page.

--- modules/nav-walker.php
+++ modules/nav-walker.php
@@ -63,8 +63,10 @@ class NavWalker extends \Walker_Nav_Menu {
     if ($this->cpt) {
       $classes = str_replace('current_page_parent', '', $classes);
 
-      if (Utils\url_compare($this->archive, $item->url)) {
-        $classes[] = 'active';
+      if ($this->archive) {
+        if (Utils\url_compare($this->archive, $item->url)) {
+          $classes[] = 'active';
+        }
       }
     }
 
