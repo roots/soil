c3d309f77051b00c9c5e14d9a2b3383437f0c0ac
Vincent Klaiber <vincent@doubledip.se>
2019-06-10 11:00:44 +0200

    Fix indentation

--- modules/disable-rest-api.php
+++ modules/disable-rest-api.php
@@ -13,13 +13,13 @@ remove_action('template_redirect', 'rest_output_link_header', 11);
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 
 add_filter('rest_authentication_errors', function ($result) {
-    if (!empty($result)) {
-        return $result;
-    }
-  
-    if (!is_user_logged_in()) {
-        return new WP_Error('rest_not_logged_in', 'You are not currently logged in.', ['status' => 401]);
-    }
-  
+  if (!empty($result)) {
     return $result;
+  }
+
+  if (!is_user_logged_in()) {
+    return new WP_Error('rest_not_logged_in', 'You are not currently logged in.', ['status' => 401]);
+  }
+
+  return $result;
 });
