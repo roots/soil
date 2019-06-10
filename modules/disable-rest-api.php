179326d15a96c0ce5868ed57984f66af8122c845
Vincent Klaiber <vinkla@users.noreply.github.com>
2019-06-10 10:55:02 +0200

    Add Gutenberg compability
    Gutenberg is using the REST-API to update content. This pull request adds a check to see if the user is logged in before restricting the API access. It basically enables Gutenberg support.

--- modules/disable-rest-api.php
+++ modules/disable-rest-api.php
@@ -13,5 +13,13 @@ remove_action('template_redirect', 'rest_output_link_header', 11);
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 
 add_filter('rest_authentication_errors', function ($result) {
-  return new \WP_Error('rest_forbidden', __('REST API forbidden.', 'soil'), ['status' => rest_authorization_required_code()]);
+    if (!empty($result)) {
+        return $result;
+    }
+  
+    if (!is_user_logged_in()) {
+        return new WP_Error('rest_not_logged_in', 'You are not currently logged in.', ['status' => 401]);
+    }
+  
+    return $result;
 });
