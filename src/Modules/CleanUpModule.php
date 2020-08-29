8ccb2e4b10d7ffa7e1e7fb81539975ecaa5ddcd8
Ben Word <ben@benword.com>
2020-08-29 15:55:21 -0600

    feat(cleanup): use filter to remove comments feed

--- src/Modules/CleanUpModule.php
+++ src/Modules/CleanUpModule.php
@@ -182,12 +182,7 @@ class CleanUpModule extends AbstractModule
      */
     protected function disableExtraRss()
     {
-        remove_action('wp_head', 'feed_links_extra', 3);
-        add_action('wp_head', 'ob_start', 1, 0);
-        add_action('wp_head', function () {
-            $pattern = '/.*' . preg_quote(esc_url(get_feed_link('comments_' . get_default_feed())), '/') . '.*[\r\n]+/';
-            echo preg_replace($pattern, '', ob_get_clean());
-        }, 3, 0);
+        add_filter('feed_links_show_comments_feed', '__return_false');
     }
 
     /**
