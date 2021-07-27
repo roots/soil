b13674216aaf46892ce1d96bb32446029605d032
QWp6t <QWp6t@users.noreply.github.com>
2021-07-27 10:12:39 -0700

    fix(autoloader): check for Roots\Soil namespace

--- src/autoload.php
+++ src/autoload.php
@@ -1,13 +1,16 @@
 <?php
 
+namespace Roots\Soil;
+
 require_once __DIR__ . '/helpers.php';
 
 spl_autoload_register(function ($className) {
+    if (strpos($className, __NAMESPACE__) !== 0) {
+        return;
+    }
     $relativeClassName = array_slice(explode('\\', $className), 2);
     $file = __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $relativeClassName) . '.php';
     if (file_exists($file)) {
-        require $file;
-        return true;
+        require_once $file;
     }
-    return false;
 });
